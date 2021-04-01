8fa3c7c6ff4f28c6b27318cf77e3455798e88750
Christian Steck <chrsteck1985@gmail.com>
2021-04-01 12:49:43 +0200

    Cleaning functions
    - Function to strip punctuation
    - Function to strip stopwords

---
+++ utils/cleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from nltk.corpus import stopwords
+stopwords=stopwords.words('german')
+
+def strip_punct(series):
+    '''Strip puncation of series of strings
+    Arguments: series - a series containing strings
+    Return: new-series - a series of strings without punctuation'''
+    new_series = series.str.replace(r'[^\w\s]+', '', regex=True)
+    return new_series
+
+
+def strip_stopwords(series, stopwords=stopwords):
+    '''Strip stopwords of series of strings
+    Arguments: series - a series containing strings, stopwords - a list of stopwords (default: german)
+    Return: new-series - a series of strings without stopwords'''
+    series=series.copy()
+    new_series = series.apply(lambda x: " ".join([word.lower() for word in x.split() if word.lower() not in (stopwords)]) if x is not None else x)
+    return new_series
+
