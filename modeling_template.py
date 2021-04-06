8c2176dd2ffd134b3ba859a7b24b863a5df50e45
Lima Tango <limatango@posteo.de>
2021-04-06 15:00:37 +0200

    Issue #23 Update template metrics
    Done:
    - Update metrics to F1, recall, precision
    - Add thoughts on train val test split

--- modeling_template.py
+++ modeling_template.py
@@ -14,6 +14,8 @@
 # ---
 
 # %%
+
+# %%
 #import sys
 #sys.path.append("..") # add project directory to system path
 
@@ -25,6 +27,7 @@ import re
 # NLP imports
 from nltk.tokenize import word_tokenize
 from nltk.stem.cistem import Cistem
+from nltk.stem.snowball import SnowballStemmer
 #from nltk.stem import GermanWortschatzLemmatizer as gwl  #https://docs.google.com/document/d/1rdn0hOnJNcOBWEZgipdDfSyjJdnv_sinuAUSDSpiQns/edit?hl=en#heading=h.oosx9e35prgf
 from nltk.corpus import stopwords
 stopwords=stopwords.words('german')
@@ -35,7 +38,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 # evaluation imports
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import f1_score, recall_score, precision_score
 
 # project utils
 from utils import loading, feature_engineering
@@ -59,6 +62,9 @@ df_posts = feature_engineering.add_column_ann_round(df_posts)
 # %%
 df = df_posts.query("ann_round == 2")
 
+# %% [markdown]
+# # TODO make function
+
 # %%
 df = df.fillna(value={"body": "", "headline": ""})
 df["text"] = df.body + df.headline
@@ -68,17 +74,25 @@ df.text = df.text.str.replace("\n", " ").str.replace("\r", " ")
 df.head()
 
 # %% [markdown]
-# ### Train test split
+# ### Train-val-test split
+#
+# Thoughts concerning the split:
+#
+# * two test sets, one sampled from annotation round 2 and the second one from round 3 (due to the sampling stategies of the authors)
+# * should we include article category/topic in stratification
+# * train-val-test split
+# * how did the authors split? 10-fold CrossValidation, 1 fold as test set for each "round"; resulting scores (stated in paper 1) are therefore CV-test-scores
+
+# %%
+df.columns
 
 # %%
 X = df.text
+y_col = ['label_argumentsused', 'label_discriminating', 'label_inappropriate', 'label_offtopic', 'label_personalstories',
+         'label_possiblyfeedback', 'label_sentimentnegative', 'label_sentimentneutral', 'label_sentimentpositive']
 y = df.label_sentimentnegative
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=24)
-
-# %%
-X_train = [s.lower() for s in X_train]
-X_test = [s.lower() for s in X_test]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=24, stratify=y)
 
 
 # %% [markdown]
@@ -108,7 +122,7 @@ def preprocess_text(text):
 
 
 # %%
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(stop_words=stopwords)
 X_train_trans = vectorizer.fit_transform(X_train)
 
 # %% [markdown]
@@ -119,9 +133,20 @@ X_train_trans = vectorizer.fit_transform(X_train)
 # #### MLFlow Setup
 
 # %%
+# !ps -A | grep gunicorn
+
+# %%
+TRACKING_URI
+
+# %%
+EXPERIMENT_NAME
+
+# %%
 # setting the MLFlow connection and experiment
 mlflow.set_tracking_uri(TRACKING_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
+
+# %%
 mlflow.start_run()
 run = mlflow.active_run()
 print("Active run_id: {}".format(run.info.run_id))
@@ -139,7 +164,11 @@ model = logreg.fit(X_train_trans, y_train)
 
 # %%
 y_train_pred = model.predict(X_train_trans)
-mse_train = mean_squared_error(y_train, y_train_pred)
+
+# %%
+mlflow.log_metric("train -" + "F1", f1_score(y_train, y_train_pred))
+mlflow.log_metric("train -" + "recall", recall_score(y_train, y_train_pred))
+mlflow.log_metric("train -" + "precision", precision_score(y_train, y_train_pred))
 
 # %% [markdown]
 # Predict on **test data**.
@@ -147,7 +176,11 @@ mse_train = mean_squared_error(y_train, y_train_pred)
 # %%
 X_test_trans = vectorizer.transform(X_test)
 y_test_pred = model.predict(X_test_trans)
-mse_test = mean_squared_error(y_test, y_test_pred)
+
+# %%
+mlflow.log_metric("test -" + "F1", f1_score(y_test, y_test_pred))
+mlflow.log_metric("test -" + "recall", recall_score(y_test, y_test_pred))
+mlflow.log_metric("test -" + "precision", precision_score(y_test, y_test_pred))
 
 # %% [markdown]
 # #### MlFlow logging
@@ -164,10 +197,10 @@ params = {
 # %%
 mlflow.log_params(params)
 mlflow.set_tag("running_from_jupyter", "True")
-mlflow.log_metric("train -" + "MSE", mse_train)
-mlflow.log_metric("test -" + "MSE", mse_test)
-mlflow.log_artifact("./models")
-mlflow.sklearn.log_model(model, "model")
+#mlflow.log_artifact("./models")
+#mlflow.sklearn.log_model(model, "model")
+path = "models/linear"
+save_model(sk_model=model, path=path)
 mlflow.end_run()
 
 # %%
