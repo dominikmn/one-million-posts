0a79a50f8973d74d660ea5ba59de514f2c620dfb
Dominik Hohmann <dominikmn@users.noreply.github.com>
2021-04-09 12:06:10 +0200

    Issue #43: Release candidate
    * Using plotly.figure_factory for creating an annotated heatmap

--- visualization_plot_f1_scores.py
+++ visualization_plot_f1_scores.py
@@ -26,7 +26,31 @@ from utils import loading, feature_engineering, scoring
 from sklearn.metrics import precision_score, recall_score, f1_score
 import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import matplotlib as plt
+
+# %% [markdown]
+# In order to use plotly from jupyterlab install the following from your terminal:
+# ```
+# jupyter labextension install jupyterlab-plotly@4.14.3
+# jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3
+# ```
+
+# %% [markdown]
+# ## Label ordering
+
+# %% tags=[]
+y_col = [
+        'label_sentimentnegative', 
+        'label_sentimentpositive',
+        'label_offtopic', 
+        'label_inappropriate', 
+        'label_discriminating', 
+        'label_possiblyfeedback', 
+        'label_personalstories', 
+        'label_argumentsused',
+    ]
 
 # %% [markdown]
 # ## Scores Zero Shot
@@ -42,26 +66,8 @@ data_1000 = data.query('ann_round==2').copy()
 # %%
 data_pred_1000 = pd.read_csv('./output/zero_shot_result_1000.csv', index_col=0)
 data_merge_1000 = pd.merge(data_1000, data_pred_1000, how='left', on = 'id_post', suffixes = ('_true', '_pred'))
-
-# %%
 scores_1000_05 = scoring.get_score_df(data_merge_1000)
 scores_1000_best = scoring.get_score_df(data_merge_1000, best=True)
-
-# %% tags=[]
-y_col = [
-        'label_sentimentnegative', 
-        'label_sentimentpositive',
-        'label_offtopic', 
-        'label_inappropriate', 
-        'label_discriminating', 
-        'label_possiblyfeedback', 
-        'label_personalstories', 
-        'label_argumentsused',
-    ]
-#[scores_1000_05.query(f"label =='{l}'")['f1_pred'].iloc[0] for l in y_col]
-
-# %% tags=[]
-scores_1000_05
 
 # %%
 scores_zeroshot = scores_1000_05[['label', 'f1_pred']].query('label in @y_col').copy()
@@ -76,9 +82,8 @@ scores_zeroshot
 
 # %%
 scores_mlflow = pd.read_csv('./output/mlflow_scores_21-04-09.csv', usecols=['label','val - F1','model',])
-
-# %%
 scores_mlflow.rename(columns={'val - F1':'f1_score'}, inplace=True)
+scores_mlflow.head(3)
 
 # %% [markdown]
 # ## Plotting the scores
@@ -88,16 +93,43 @@ scores_all = pd.concat([scores_zeroshot, scores_mlflow]).reset_index(drop=True) 
 scores_all.shape
 
 # %%
-scores_pivot = scores_all.pivot_table(index='label', columns='model', values='f1_score')#.reset_index()
-#scores_pivot.loc[y_col[1],'BOW']
-#scores_pivot.set_index('label')
-scores_pivot
+scores_pivot = scores_all.pivot_table(index='label', columns='model', values='f1_score').reindex(y_col[::-1]) #reindex gives a custom order to the rows
+
 
 # %%
-sns.heatmap(scores_pivot)
+scores = scores_pivot.iloc[:,[5,4,1,2]]  # choose only specific columns (i.e. models)
 
 # %%
-fig = px.imshow(scores_pivot)
+z      = np.array(scores.reset_index(drop=True)) # Must be a np.array instead of pandas DataFrame
+z_text = [["{:.2f}".format(y) for y in x] for x in np.array(scores.reset_index(drop=True))]
+x      = list(scores.columns) # Must be a list instead of pandas Series
+y      = list(scores.index) # Must be a list instead of pandas Series
+
+# %%
+layout_heatmap = go.Layout(
+    title=('Model f1 scores'),
+    #xaxis=dict(title='Models'), 
+    yaxis=dict(title='Labels', dtick=1),
+    paper_bgcolor='rgba(255,255,255,255)',
+    plot_bgcolor='rgba(255,255,255,255)',
+)
+
+ff_fig = ff.create_annotated_heatmap(
+    z= z,
+    x= x,
+    y= y,
+    annotation_text=z_text, 
+    colorscale=[[0, '#d3d3d3'],[1, '#ec008e']],
+    showscale = True,
+    ygap=5,
+    xgap=0,
+)
+fig  = go.FigureWidget(ff_fig)
+fig.layout=layout_heatmap
+fig.layout.annotations = ff_fig.layout.annotations
+fig.data[0].colorbar = dict(title='f1 Score', titleside = 'right')
+fig.update_layout(title_x=0.5)
+fig.update_xaxes(side="top")
 fig.show()
 
 # %%
