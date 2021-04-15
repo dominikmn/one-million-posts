3b0dd88fadda3d30cb2ad7a43f4bf4b44c37fcd9
Lima Tango <limatango@posteo.de>
2021-04-15 13:30:21 +0200

    Issue #59 Add example params for grid search

--- modeling/nb.py
+++ modeling/nb.py
@@ -17,13 +17,21 @@ if __name__ == "__main__":
         ("vectorizer", CountVectorizer()),
         ("clf", MultinomialNB()),
     ])
+    param_grid = {
+        "vectorizer__ngram_range" : [(1,1), (1,2), (1,3)],
+        "vectorizer__stop_words" : [stopwords, None],
+        "vectorizer__min_df": np.linspace(0, 0.1, 3),
+        "vectorizer__max_df": np.linspace(0.9, 1.0, 3),
+    }
+    gs = GridSearchCV(pipeline, param_grid, scoring="f1", cv=3, verbose=1)
+
     TARGET_LABELS = ['label_argumentsused', 'label_discriminating', 'label_inappropriate',
         'label_offtopic', 'label_personalstories', 'label_possiblyfeedback',
         'label_sentimentnegative', 'label_sentimentpositive',]
     
     data = modeling.Posts()
-    training = modeling.Training(data, pipeline)
-    for label in TARGET_LABELS:
+    training = modeling.Training(data, gs)
+    for label in TARGET_LABELS[:1]:
         data.set_label(label)
         training.train()
         training.evaluate(["train", "val"])
