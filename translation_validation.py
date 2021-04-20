1193ec946e6a491920ff8b1dbfea9fe9a9ba3a6b
Dominik Hohmann <dominikmn@users.noreply.github.com>
2021-04-20 18:02:40 +0200

    Issue #85: Check translations of train-sets: inappropriate, off-topic.

--- translation_validation.py
+++ translation_validation.py
@@ -1,5 +1,9 @@
+# # Review validation sets and train sets #85
+# Issue link: https://github.com/dominikmn/one-million-posts/issues/85
+
 import pandas as pd
-from utils import feature_eng
+
+# ## Validation sets
 
 df_val_disc = pd.read_csv('./output/trans_val_label_discriminating.csv')
 
@@ -51,5 +55,64 @@ for i in df_val_ina.sort_values('id_post')[['text','lang','orig']].iterrows():
 # Conclusion:
 #
 # There are some inconsistencies and problems in the back-translations, but in general they seem to be okay. We accept them. Additional task: Add further positive examples from round 3.
+
+# ## Train sets
+
+import random
+
+# ### Inaproppriate
+
+df_train_ina = pd.read_csv('./output/trans_label_inappropriate.csv')
+
+df_train_ina = df_train_ina.fillna(value={"body": "", "headline": ""})
+df_train_ina["orig"] = df_train_ina.headline + " " + df_train_ina.body
+df_train_ina.orig = df_train_ina.orig.str.replace("\n", " ").str.replace("\r", " ")
+
+batch= random.randint(0,df_train_ina.shape[0]/7)
+num_of_lang = 7
+for n, i in enumerate(df_train_ina.sort_values('id_post')[['text','lang','orig']].iterrows()):
+    if num_of_lang*batch < n < num_of_lang*(batch+1):
+        print(list(i[1]))
+        print()
+        print()
+
+
+# Languages used:
+# * Filipino	fil -> not so well
+# * Japanese	ja -> okay
+# * German	de -> (obvious)
+# * Maori 	mi -> Works well
+# * Samoan	sm -> poor
+# * Afrikaans	af (no clear tendency)
+# * Kannada	kn -> good
+
+Topics:
+* Migration
+* Insult
+
+# ### Off-topic
+
+df_train_off = pd.read_csv('./output/trans_label_offtopic.csv')
+
+df_train_off = df_train_off.fillna(value={"body": "", "headline": ""})
+df_train_off["orig"] = df_train_off.headline + " " + df_train_off.body
+df_train_off.orig = df_train_off.orig.str.replace("\n", " ").str.replace("\r", " ")
+
+num_of_lang = 5
+batch= random.randint(0,df_train_off.shape[0]/num_of_lang)
+for n, i in enumerate(df_train_off.sort_values('id_post')[['text','lang','orig']].iterrows()):
+    if num_of_lang*batch < n < num_of_lang*(batch+1):
+        print(list(i[1]))
+        print()
+        print()
+
+
+# Languages used:
+# * Filipino	fil -> not so well
+# * Japanese	ja -> okay
+# * Maori 	mi -> 
+# * Samoan	sm -> very poor (even worse than ina)
+# * Afrikaans	af
+# * Kannada	kn -> good
 
 
