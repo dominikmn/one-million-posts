2ab9e6a980968764c7c545037e142417ab0b27e8
Lima Tango <limatango@posteo.de>
2021-04-20 17:20:49 +0200

    Issue #79 Add interactive textfield and update style

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 
 from utils import loading
 
-data = loading.load_extended_posts()
-data_plot = data.groupby("id_article")["id_post"].count().reset_index()
+#data = loading.load_extended_posts()
+#data_plot = data.groupby("id_article")["id_post"].count().reset_index()
 
 external_stylesheets = [
     {
@@ -15,32 +16,88 @@ external_stylesheets = [
         "rel": "stylesheet",
     },
 ]
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
+#server = app.server
 app.title = "One Million Posts"
 
 app.layout = html.Div(
     children = [
-        html.H1(children="One Million Posts",),
-        html.P(children="Annotate newspaper posts regarding negative-sentiment, off-topic, discrimination, and inappropriateness.",),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data_plot["id_article"],
-                        "y": data_plot["id_post"],
-                        "type": "dots",
-                    },
-                ],
-                "layout": {
-                    "title": "Number of posts for each Article",
-                },
-            }
-        )
+        html.Div(
+            children=[
+                html.H1(children="One Million Posts", className="header-title"),
+                html.P(
+                    children="Annotate newspaper posts regarding negative-sentiment, off-topic, discrimination, and inappropriateness.",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                #html.Div(
+                    #children=dcc.Graph(
+                        #id="post-per-article-chart",
+                        #config={"displayModeBar": False},
+                        #figure={
+                            #"data": [
+                                #{
+                                    #"x": data_plot["id_article"],
+                                    #"y": data_plot["id_post"],
+                                    #"type": "lines",
+                                    #"hovertemplate": "%{y:.2f}" "<extra></extra>",
+                                #},
+                            #],
+                            #"layout": {
+                                #"title": {
+                                    #"text": "Number of posts for each Article",
+                                    #"x": 0.05,
+                                    #"xanchor": "left",
+                                #},
+                                #"xaxis": {
+                                    #"text": "Article ID",
+                                    #"fixedrange": True,
+                                #},
+                                #"yaxis": {
+                                    #"text": "Number of posts",
+                                    #"fixedrange": True,
+                                #},
+                                #"colorway": ["#17B897"],
+                            #},
+                        #},
+                    #),
+                    #className="card",
+                #),
+                html.Div(
+                    children=[
+                        html.H6(
+                            children="Enter the post that shall be analysed:",
+                        ),
+                        html.Div(
+                            dcc.Textarea(
+                                id="post-input",
+                                value="",
+                                style={"width": "100%", "height": 100},
+                            ),
+                        ),
+                        html.Br(),
+                        html.Div(id="post-output", style={'whiteSpace': 'pre-line'}),
+                    ],
+                ),
+            ],
+        className="wrapper",
+        ),
     ]
 )
 
+@app.callback(
+    [Output(component_id="post-output", component_property="children")],
+    [Input(component_id="post-input", component_property="value")],
+)
+def update_output_div(input_value):
+    return ["Output: {}".format(input_value)]
+
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
