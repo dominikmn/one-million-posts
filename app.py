6fe21457da2ad3278baf55ff5688afdbb06e5764
Lima Tango <limatango@posteo.de>
2021-04-23 19:49:38 +0200

    Issue #79 Update texts, layouts and callbacks

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 
 from utils import loading
 COLORS =[[0, '#d3d3d3'],[1, '#ec008e']]
-
+COLOR_PINK = "#ec008e"
 #data = loading.load_extended_posts()
 #data_plot = data.groupby("id_article")["id_post"].count().reset_index()
 
@@ -34,8 +34,8 @@ app.layout = html.Div(
         html.Div(
             children=[
                 html.H1(children="One Million Posts", className="header-title"),
-                html.P(
-                    children="Annotate newspaper posts regarding negative-sentiment, off-topic, discrimination, and inappropriateness.",
+                html.H6(
+                    children="Assisting newspaper moderators with machine learning",
                     className="header-description",
                 ),
             ],
@@ -43,83 +43,43 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
-                #html.Div(
-                    #children=dcc.Graph(
-                        #id="post-per-article-chart",
-                        #config={"displayModeBar": False},
-                        #figure={
-                            #"data": [
-                                #{
-                                    #"x": data_plot["id_article"],
-                                    #"y": data_plot["id_post"],
-                                    #"type": "lines",
-                                    #"hovertemplate": "%{y:.2f}" "<extra></extra>",
-                                #},
-                            #],
-                            #"layout": {
-                                #"title": {
-                                    #"text": "Number of posts for each Article",
-                                    #"x": 0.05,
-                                    #"xanchor": "left",
-                                #},
-                                #"xaxis": {
-                                    #"text": "Article ID",
-                                    #"fixedrange": True,
-                                #},
-                                #"yaxis": {
-                                    #"text": "Number of posts",
-                                    #"fixedrange": True,
-                                #},
-                                #"colorway": ["#17B897"],
-                            #},
-                        #},
-                    #),
-                    #className="card",
-                #),
+                html.P(
+                    children="Enter your post:",
+                ),
                 html.Div(
-                    children=[
-                        html.H6(
-                            children="Enter the post that shall be analysed:",
-                        ),
-                        html.Div(
-                            dcc.Textarea(
-                                id="post-input",
-                                value="",
-                                style={"width": "100%", "height": 100},
-                            ),
-                        ),
-                        html.Button(id='submit-state', n_clicks=0, children='Predict'),
-                        html.Br(),
-                        html.Div(id="post-output", style={'whiteSpace': 'pre-line'}),
-                        html.Div(id="post-prediction", style={'whiteSpace': 'pre-line'}),
-                        html.Div(
-                            children = dcc.Graph(
-                                id="prediction-chart", config={"displayModeBar": False},
-                            ),
-                            id="display-prediction-chart",
-                            style={"visibility": "hidden"},
-                            className="card",
-                        ),
-                    ],
+                    dcc.Textarea(
+                        id="post-input",
+                        value="",
+                        style={"width": "100%", "height": 100},
+                    ),
+                ),
+                html.Button(id='submit-state', n_clicks=0, children='Analyse'),
+                html.H6(id="post-prediction", style={'whiteSpace': 'pre-line'}),
+                html.Div(
+                    children = dcc.Graph(
+                        id="prediction-chart", config={"displayModeBar": False},
+                    ),
+                    id="display-prediction-chart",
+                    style={"visibility": "hidden"},
+                    className="card",
                 ),
             ],
         className="wrapper",
         ),
+        html.Div(
+            children=[
+                html.Footer("https://github.com/dominikmn/one-million-posts/"),
+            ],
+            className="footer",
+        )
     ]
 )
 
 
 @app.callback(
-    [Output(component_id="post-output", component_property="children")],
-    [Input(component_id="post-input", component_property="value")],
-)
-def update_output_div(input_value):
-    return ["Output: {}".format(input_value)]
-
-
-@app.callback(
     [
         Output(component_id="post-prediction", component_property="children"),
+        Output(component_id="post-prediction", component_property="style"),
         Output(component_id="display-prediction-chart", component_property="style"),
         Output(component_id="prediction-chart", component_property="figure"),
     ],
@@ -132,7 +92,11 @@ def update_prediction(n_clicks, input_value):
         response = requests.post('http://127.0.0.1:8000/predict', json=new_measurement)
         if response.ok:
             result = response.json()
-            mapping = {0: "does not need", 1: "needs"}
+            mapping = {0: "Everything's fine", 1: "Needs moderation"}
+            style_prediction = {
+                0: {"color": "black"},
+                1: {"color": COLOR_PINK},
+            }
             style_prediction_chart = {
                 0: {"visibility": "hidden"},
                 1: {"visibility": "visible"},
@@ -141,14 +105,16 @@ def update_prediction(n_clicks, input_value):
             predictions.pop("needsmoderation")
             df = get_df_from_predictions(predictions)
             return (
-                [f"This post {mapping[result['needsmoderation']]} moderation. NegSent {result['sentimentnegative']}. Inapp {result['inappropriate']}. Disc {result['discriminating']}."],
+                [f"{mapping[result['needsmoderation']]}"],
+                style_prediction[result["needsmoderation"]],
                 style_prediction_chart[result["needsmoderation"]],
                 update_prediction_chart(df),
             )
     predictions = {'sentimentnegative': 0.0, 'inappropriate': 0.0, 'discriminating': 0.0}
     df = get_df_from_predictions(predictions)
     return (
-        [f"Uuups, something went wrong. Did you enter a text?"],
+        [f"No text provided"],
+        {"color": "black"},
         {"visibility": "hidden"},
         update_prediction_chart(df),
     )
@@ -167,8 +133,27 @@ def update_prediction_chart(long_df):
         y=long_df["category"],
         orientation="h",
         marker_color=colors,
+        hovertemplate="%{x}<extra></extra>",
     )])
-    fig.layout.template = "none"
+    fig.layout = {
+        "template": "none",
+        "height": 250,
+        "margin": {
+            "t": 25,
+            "r": 5,
+            "b": 60,
+        },
+        "xaxis": {
+            "title": "Probability",
+            "tickformat": "%",
+        },
+        "yaxis": {
+            "automargin": True,
+            "tickvals": ["discriminating", "inappropriate", "sentimentnegative"],
+            "ticktext": ["Discriminating ", "Inappropriate ", "Negative Sentiment "],
+        },
+    }
+
     return fig
 
 if __name__ == "__main__":
